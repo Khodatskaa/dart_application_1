e3038c33289dd8276148782e544c85dc17fb46ca
Khodatskaa <khodatskaalina@gmail.com>
2023-12-15 11:40:36 +0200

    Ex 3 was done

--- lib/domain/entities/play/demonstration.dart
+++ lib/domain/entities/play/demonstration.dart
@@ -1,8 +1,6 @@
-//import 'package:dart_application_1/domain/entities/player/mp3player.dart';
 import 'package:dart_application_1/domain/entities/player/videoplayer.dart';
 
 void main() {
-  //final mp3Player = MP3Player();
   final videoPlayer = VideoPlayer();
 
   void playMedia(dynamic media) {
