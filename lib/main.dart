83dec6c827f562048658d6cf6a87823e23c7944e
Khodatskaa <khodatskaalina@gmail.com>
2024-01-23 18:39:03 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+void main() {
+  print('CV');
+  print('Name: Alina');
+  print('Surname: Khodatska');
+  print('Age: 16');
+  print('Education: 11th grade, IT courses');
+  print('Languages: Ukrainian, English');
+  print('Skills: HTML, CSS, Python, C++');
+}
